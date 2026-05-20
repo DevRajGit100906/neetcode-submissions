fbfd0b72ed647a47770088eb488326f695a97e2a
Dev Raj Das <cs1240103@iitd.ac.in>
2026-05-20 23:20:14 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> s1(26, 0), t1(26, 0);
+        int n = s.size(), m = t.size();
+        if (n != m) return false;
+        for(int i = 0; i < n; i++) {
+            s1[s[i]-'a']++;
+            t1[t[i]-'a']++;
+        };
+        for(int i = 0; i < 26; i++) {
+            cout << s1[i] << " " << t1[i] << "YEE";
+            if (s1[i] != t1[i]) return false;
+        };
+        return true;
+    }
+};
